41b0b18395d2a4a49003a5bc9fc3c08d47af94a9
KLL3o <162317328+KLL3o@users.noreply.github.com>
2025-08-10 22:52:06 -0300

    Add files via upload

---
+++ Base de datos.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF NOT EXISTS gimnasio;
+CREATE DATABASE gimnasio;
+USE gimnasio;
+
+
+CREATE TABLE gimnasio (
+    id_gimnasio SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    direccion TEXT,
+    telefono VARCHAR(30)
+);
+
+CREATE TABLE culturistas (
+    id_culturista SERIAL PRIMARY KEY,
+    numero_socio VARCHAR(50) ,
+    dni VARCHAR(20),
+    nombre VARCHAR(120),
+    telefono VARCHAR(30)
+);
+CREATE TABLE administrador (
+    id_admin  PRIMARY KEY,
+    nombre VARCHAR(120) NOT NULL,
+    edad INT,
+    telefono VARCHAR(30)
+);
